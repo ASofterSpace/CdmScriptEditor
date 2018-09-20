0b769fb3a6137e39d0260da3ff73f1dc49000d59
Moyaccercchi <moya@asofterspace.com>
2018-09-20 02:24:22 +0000

    Increase Heap for huge CDMs

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-java -classpath "bin;emf/*" -Xms16m -Xmx1024m com.asofterspace.cdmScriptEditor.Main
+java -classpath "bin;emf/*" -Xms16m -Xmx4096m com.asofterspace.cdmScriptEditor.Main
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -classpath "bin:emf/*" -Xms16m -Xmx1024m com.asofterspace.cdmScriptEditor.Main
+java -classpath "bin:emf/*" -Xms16m -Xmx4096m com.asofterspace.cdmScriptEditor.Main
