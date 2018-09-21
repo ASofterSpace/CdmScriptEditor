a2f72aa738196a15982815ec5e5ce8f6f1fccfeb
Moya <moyaccercchi@hotmail.de>
2018-09-21 12:57:50 +0200

    Adjust heap space down for 32bit Java versions

--- run.bat
+++ run.bat
@@ -1,1 +1,17 @@
-java -classpath "bin;emf/*" -Xms16m -Xmx4096m com.asofterspace.cdmScriptEditor.Main
+@echo off
+
+REM   check if we are running a 64-Bit version of Java
+java -version 2>&1 | find "64-Bit"
+
+if errorlevel 1 goto :32bit
+
+:64bit
+REM   we are running 64 bit - nice! assign loads of heap!
+java -classpath "bin;emf/*" -Xms16m -Xmx4096m com.asofterspace.cdmScriptEditor.Main %*
+goto :end
+
+:32bit
+REM   we are running 32 bit - oh no! assign only a tinly little bit of heap!
+java -classpath "bin;emf/*" -Xms16m -Xmx1024m com.asofterspace.cdmScriptEditor.Main %*
+
+:end
--- run.sh
+++ run.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-java -classpath "bin:emf/*" -Xms16m -Xmx4096m com.asofterspace.cdmScriptEditor.Main
+# check if we are running a 64-Bit version of Java
+javaversion=$(java -version 2>&1)
+
+if [[ $javaversion = *"64-Bit"* ]]; then
+	# we are running 64 bit - nice! assign loads of heap!
+	java -classpath "bin:emf/*" -Xms16m -Xmx4096m com.asofterspace.cdmScriptEditor.Main "$@"
+else
+	# we are running 32 bit - oh no! assign only a tinly little bit of heap!
+	java -classpath "bin:emf/*" -Xms16m -Xmx1024m com.asofterspace.cdmScriptEditor.Main "$@"
+fi
