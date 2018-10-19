fd8ebcfa8af1bc39cc043777a57ee8d0f6b4cdd9
Moya <moyaccercchi@hotmail.de>
2018-10-19 15:38:40 +0200

    Take out old emf nonsense some more

--- run.bat
+++ run.bat
@@ -7,11 +7,11 @@ if errorlevel 1 goto :32bit
 
 :64bit
 REM   we are running 64 bit - nice! assign loads of heap!
-java -classpath "bin;emf/*" -Xms16m -Xmx4096m com.asofterspace.cdmScriptEditor.Main %*
+java -classpath "bin" -Xms16m -Xmx4096m com.asofterspace.cdmScriptEditor.Main %*
 goto :end
 
 :32bit
 REM   we are running 32 bit - oh no! assign only a tinly little bit of heap!
-java -classpath "bin;emf/*" -Xms16m -Xmx1024m com.asofterspace.cdmScriptEditor.Main %*
+java -classpath "bin" -Xms16m -Xmx1024m com.asofterspace.cdmScriptEditor.Main %*
 
 :end
